720eeadac8a1deb0356dafd73fd799ff9cb7e7ea
jkuns76 <jkoonman@gmail.com>
2022-02-21 18:19:24 -0600

    excercise 19

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,8 +1,11 @@
+SELECT FirstName, LastName, max(Total) total_sales
+FROM(
 SELECT
     e.FirstName,
     e.LastName,
-    SUM(Total) total_sales
+    SUM(Total) AS total
 FROM Invoice i
 JOIN Customer c ON c.CustomerID = i.CustomerID
 JOIN Employee e ON e.EmployeeID = c.SupportRepId
-GROUP BY EmployeeId
+WHERE i.InvoiceDate LIKE '2009%'
+GROUP BY e.EmployeeId)
